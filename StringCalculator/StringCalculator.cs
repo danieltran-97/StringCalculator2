860641ece300336a3bd387c8fac3b1bf2ddadf7f
Daniel Tran <59809773+danieltran-myob@users.noreply.github.com>
2021-06-03 14:55:42 +1000

    upload changes

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            int output = 0;
+
+            var isNullOrEmpty = String.IsNullOrEmpty(input);
+            var hasCommas = input.Contains(',');
+            
+            if (hasCommas)
+            {
+                output = HandleStringWithCommas(input);
+            }
+            if (isNullOrEmpty && !hasCommas)
+            {
+                output = 0;
+            }
+            if (!isNullOrEmpty && !hasCommas)
+            { 
+                output = Convert.ToInt32(input);
+            } 
+            
+            return output;
+        }
+
+        public int HandleStringWithCommas(string input)
+        {
+            var sum = 0;
+            
+            var inputList = input.Replace("\n",",").Split(",");
+
+            foreach (var number in inputList)
+            {
+                sum += int.Parse(number);
+            }
+
+            return sum;
+        }
+    }
+}
